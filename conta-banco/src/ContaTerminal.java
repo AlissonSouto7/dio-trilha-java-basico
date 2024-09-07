9623d1e73915f263b8b225f40c6687193efc27b1
Alisson Souto <alissonprogp1@gmail.com>
2024-09-07 20:42:23 -0300

    Classe ContaTerminal Criada passando todos os parametros do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor digite o seu nome");
+        String nomeCliente = scanner.next();   
+        
+        System.out.println("Digite o número da agência");
+        String agencia = scanner.next();
+
+        System.out.println("Insira o número da conta");
+        int numero = scanner.nextInt();
+
+        System.err.println("Insira o seu saldo da conta");
+        double saldo = scanner.nextDouble();
+
+        System.err.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
